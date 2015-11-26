f08d1b91e71e5d57ecb0bfe4a0db2a3834b3926d
Izabella Konstanciak <i.konstanciak@gmail.com>
2015-11-26 17:40:19 +0100

    Extract todo list helper

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -2,14 +2,7 @@ require 'spec_helper'
 
 describe "Viewing todo items" do
   let!(:todo_list) {TodoList.create(title: "Grocery list", description: "Groceries")}
-  
-  def visit_todo_list(list)
-    visit "/todo_lists"
-    within "#todo_list_#{list.id}" do
-      click_link "List Items"
-    end
-  end
-  
+    
   #before block: it will do all this before each scenario
   #before do
   #end
